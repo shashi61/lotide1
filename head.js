e7cf14548be3216c831f89a8cd3d1050bfa17320
Shashi Bawa <shashibawa104@gmail.com>
2021-11-30 00:16:52 +0000

    Refactored files

--- head.js
+++ head.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
 const head = function(arrayToTest){
     if(arrayToTest.length === 0){
         return undefined;
@@ -15,6 +8,4 @@ const head = function(arrayToTest){
     return arrayToTest[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
