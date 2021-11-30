2466013ffc737ef94bd69ced54f86c1b70d3a4ee
Shashi Bawa <shashibawa104@gmail.com>
2021-11-30 01:10:46 +0000

    Refactored code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,4 @@
-const eqArrays = function(value1, value2){
-    let flag = true;
-    if(value1.length === value2.length){
-    for(let i = 0; i < value1.length; i++){
-        if(value1[i] !== value2[i]){
-         flag = false;
-        }  
-    }
-    if(flag === true){
-        return true;
-    }
-    else{
-    return false;
-    }
-}
-else{
-    return false;
-}
-}
+const eqArrays = require("./eqArrays");
 const assertArraysEqual = function(value1, value2){
    if(eqArrays(value1, value2)){
     console.log(`✅✅✅ Assertion Passed: ${value1} === ${value2}`);
@@ -24,5 +6,4 @@ const assertArraysEqual = function(value1, value2){
     console.log(`🛑🛑🛑 Assertion Failed: ${value1} !== ${value2}`);
    }  
 }
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,1],[1,2,3]);
+module.exports = assertArraysEqual;
